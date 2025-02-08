2968dd8e6f387f4665b368c9cf0d16e91f1207d0
Luis Alcaras <l.alcaras@f5.com>
2025-02-07 19:22:48 -0600

    fix: Azael picture has been fixed

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -2,7 +2,7 @@ export const Picture = () => {
   return (
     <div className="mx-auto text-center px-4 py-4 font-im-fell-english-sc">
       <div className="mx-auto w-64 md:w-[50%] border-4 border-amber-950 aspect-square  overflow-hidden">
-        <img src="/pictures/azael.jpeg" alt="Azael" className="sepia-[0.85]" />
+        <img src="pictures/azael.jpeg" alt="Azael" className="sepia-[0.85]" />
       </div>
       <div
         className="font-rye text-xl md:text-2xl uppercase text-amber-950"
